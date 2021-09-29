58a01c08fb456f2753d65471abf37394bbf4badc
Arquery <sgu_sigu@msn.com>
2021-09-29 10:36:23 -0600

    Add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData = [
 	{name: 'Gustavo'},
 	{name: 'Emilio'}
 ];
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+});
 
 // In Express, words with a colon in front of them in the url are treated as variables.
 app.get('/users/:id', function(req, res){
@@ -14,6 +25,28 @@ app.get('/users/:id', function(req, res){
 		message: 'Got one user',
 		users: req.params.id
 	})
+});
+
+app.post('/login', function(req, res){
+	const username = req.body.username;
+	const password = req.body.password;
+
+	const mockUsername = "billyTheKid";
+	const mockPassword = "superSecret";
+
+	if(username === mockUsername && password === mockPassword){
+		res.json({
+			success: true,
+			message: 'Password and username match!',
+			token: 'encrypted token goes here'
+		});
+	}
+	else{
+		res.json({
+			success: false,
+			message: 'Password and username do not match'
+		});
+	}
 });
 
 app.listen(8000, function(){
