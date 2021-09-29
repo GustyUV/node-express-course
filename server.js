28ebcccf4f804806ae077362c146f21eec10d3cf
Arquery <sgu_sigu@msn.com>
2021-09-28 18:12:42 -0600

    Add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+	{name: 'Gustavo'},
+	{name: 'Emilio'}
+];
+
+app.get('/users', function(req, res){
+	res.json({
+		success: true,
+		message: 'Successfully got users"',
+		users: mockUserData
+	})
+});
+
 app.listen(8000, function(){
 	console.log("Server is running...");
 });
